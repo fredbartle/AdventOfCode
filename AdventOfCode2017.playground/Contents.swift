850d4c9943835f28c50675df8369dba0d31cc096
Fred Bartle <Fred.Bartle@vtinfo.com>
2017-12-01 15:01:26 -0500

    Day 1 part 2

--- AdventOfCode2017.playground/Contents.swift
+++ AdventOfCode2017.playground/Contents.swift
@@ -4,26 +4,21 @@ import UIKit
 
 let day1input = input("day1input")
 
-func captchaSum(_ text: String) -> Int {
-    guard let firstChar = text.first else { return 0 }
-    var text = text
-    text.append(firstChar)
-    var previousChar: Character?
-    return text
-        .flatMap { char in
-            if previousChar == nil {
-                previousChar = char
-                return nil
-            } else if char == previousChar {
-                return String(char)
-            } else {
-                previousChar = char
-                return nil
-            }
+func captchaSum(_ text: String, offset by: Int = 1) -> Int {
+    let shifted = Array(text).shiftRight(by).map { String($0) }.joined()
+
+    var sum = 0
+    for (i, char) in text.enumerated() {
+        let index = shifted.index(shifted.startIndex, offsetBy: i)
+        if char == shifted[index],
+            let value = Int(String(char)) {
+            sum += value
         }
-        .flatMap { Int($0) }
-        .reduce(0, { $0 + $1 })
+    }
+    return sum
 }
+
+// part 1
 
 assert(captchaSum("1122") == 3)
 assert(captchaSum("1111") == 4)
@@ -32,3 +27,12 @@ assert(captchaSum("91212129") == 9)
 
 captchaSum(day1input)
 
+// part 2
+
+assert(captchaSum("1212", offset: 2) == 6)
+assert(captchaSum("1221", offset: 2) == 0)
+assert(captchaSum("123425", offset: 3) == 4)
+assert(captchaSum("123123", offset: 3) == 12)
+assert(captchaSum("12131415", offset: 4) == 4)
+
+captchaSum(day1input, offset: day1input.count / 2)
