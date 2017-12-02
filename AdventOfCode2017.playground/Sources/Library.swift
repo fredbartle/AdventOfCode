f8af4a23dc78b630240d6aa33336b34b86c66687
Fred Bartle <fredbartle@gmail.com>
2017-12-02 08:01:54 -0500

    [2017] Fixed filename.

--- AdventOfCode2017.playground/Sources/Library.swift
+++ AdventOfCode2017.playground/Sources/Library.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public func input(_ filename: String) -> String {
-    let fileURL = Bundle.main.url(forResource: "day1input", withExtension: nil)
+    let fileURL = Bundle.main.url(forResource: filename, withExtension: nil)
     do {
         return try String(contentsOf: fileURL!).trimmingCharacters(in: .whitespacesAndNewlines)
     }
