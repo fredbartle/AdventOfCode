7bee7b1786a6a55e23e02e21919ddcfb42214943
Fred Bartle <fredbartle@gmail.com>
2017-12-02 08:05:43 -0500

    [2017] Moved archive to Resources so it will not be compiled.

--- AdventOfCode2017.playground/Resources/Day1.swift
+++ AdventOfCode2017.playground/Resources/Day1.swift
@@ -1,5 +1,3 @@
-/*
-
 let day1input = input("day1input")
 
 func captchaSum(_ text: String, offset by: Int = 1) -> Int {
@@ -31,5 +29,3 @@ assert(captchaSum("123123", offset: 3) == 12)
 assert(captchaSum("12131415", offset: 4) == 4)
 
 captchaSum(day1input, offset: day1input.count / 2)
-
-*/
